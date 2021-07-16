fc6571f9d1bea161a3ecfdfc1f74e95bb6d4d880
Moon Chung <koreas1337@hotmail.com>
2021-07-16 21:32:59 +0000

    Removed my appId and password and changed node count back to 3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,13 +3,6 @@
 # variable "serviceprinciple_key" {}
 # variable "tenant_id" {}
 
-variable "appId" {
-    default = "f47a811a-8ef4-480b-9038-a08b6116d0c7"
-}
-variable "password" {
-    default = "ad2db723-0b3b-46d9-9849-9a72cced79df"
-}
-
 variable "resource_group" {
 # Replace with your own values if this is used as a template
     type = map
@@ -24,7 +17,7 @@ variable "kubernetes_cluster_dev" {
     default = {
         "name"              = "may24_devops_aks_dev"
         "dns_prefix"        = "may24-devops-dev"
-        "node_count"        = 1
+        "node_count"        = 3
         "node_pool_name"    = "development"
     }
 }
@@ -34,7 +27,7 @@ variable "kubernetes_cluster_staging" {
     default = {
         "name"              = "may24_devops_aks_staging"
         "dns_prefix"        = "may24-devops-staging"
-        "node_count"        = 1
+        "node_count"        = 3
         "node_pool_name"    = "staging"        
     }
 }
