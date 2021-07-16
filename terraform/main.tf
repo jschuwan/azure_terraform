1b69e5292bb4889806d47920e88150c572f93500
Moon Chung <koreas1337@hotmail.com>
2021-07-16 19:37:54 +0000

    Shoved contents of outputs.tf back into main.

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,3 +83,30 @@ resource "azurerm_kubernetes_cluster" "may24_devops_staging" {
         CreatedDate             = timestamp()
     }
 }
+
+output "kube_config_dev" {
+    value = azurerm_kubernetes_cluster.may24_devops_dev.kube_config_raw
+    sensitive = true
+}
+
+output "kube_config_staging" {
+    value = azurerm_kubernetes_cluster.may24_devops_staging.kube_config_raw
+    sensitive = true
+}
+
+# terraform output configure
+# Probably want this in a separate outputs.tf file?
+output "configure" {
+    value = <<CONFIGURE
+
+Run the following commands to configure the kubernetes client:
+
+terraform output kube_config_dev > ~/.kube/may24_devops_config_dev
+terraform output kube_config_staging > ~/.kube/may24_devops_config_staging
+export KUBECONFIG=~/.kube/may24_devops_config
+
+Test configuration using kubectl:
+
+kubectl get nodes
+CONFIGURE
+}
