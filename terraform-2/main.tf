ab96a8eff47a438bef6ca60540eca8da03cbfedc
Moonjun Chung <moonjchung@gmail.com>
2021-07-16 11:29:14 -0400

    moon terraform

---
+++ terraform-2/main.tf
@@ -0,0 +1,95 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "=2.60.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "may24_devops" {
+    name        = var.resource_group["name"]
+    location    = var.resource_group["location"]
+}
+
+resource "azurerm_kubernetes_cluster" "may24_devops_dev" {
+    name                = var.kubernetes_cluster_dev["name"]
+    location            = azurerm_resource_group.may24_devops.location
+    resource_group_name = azurerm_resource_group.may24_devops.name
+    dns_prefix          = var.kubernetes_cluster_dev["dns_prefix"]
+
+    default_node_pool {
+        name                = "default"
+        node_count          = var.kubernetes_cluster_dev["node_count"]
+        vm_size             = "Standard_DS2_v2"
+        #enable_auto_scaling = false
+    }
+
+# Can be used with Service principal for my own Azure Service but how for Nick's?
+    service_principal {
+      client_id     = var.appId
+      client_secret = var.password
+    }
+
+    role_based_access_control {
+      enabled = true
+    }
+
+    tags = {
+        environment = "dev"
+    }
+}
+
+resource "azurerm_kubernetes_cluster" "may24_devops_staging" {
+    name                = var.kubernetes_cluster_staging["name"]
+    location            = azurerm_resource_group.may24_devops.location
+    resource_group_name = azurerm_resource_group.may24_devops.name
+    dns_prefix          = var.kubernetes_cluster_staging["dns_prefix"
+    default_node_pool {
+        name                = "default"
+        node_count          = var.kubernetes_cluster_staging["node_count"]
+        vm_size             = "Standard_DS2_v2"
+        #enable_auto_scaling = false
+    
+    service_principal {
+      client_id     = var.appId
+      client_secret = var.password
+    
+    role_based_access_control {
+      enabled = true
+    
+    identity {
+        type = "SystemAssigned"
+    }
+
+    tags = {
+        environment = "dev"
+    }
+}
+
+# Provides the config file to connect to AKS cluster
+output "kube_config" {
+    value = azurerm_kubernetes_cluster.may24_devops_dev.kube_config_raw
+}
+
+# terraform output configure
+# Probably want this in a separate outputs.tf file?
+output "configure" {
+    value = <<CONFIGURE
+
+Run the following commands to configure the kubernetes client:
+
+terraform output kube_config > ~/.kube/may24_devops_config
+export KUBECONFIG=~/.kube/may24_devops_config
+
+Test configuration using kubectl:
+
+kubectl get nodes
+CONFIGURE
+}
+
+#module {}
