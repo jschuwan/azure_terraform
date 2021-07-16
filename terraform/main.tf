561bf239e39987e077a31ac426be5f9c223ff11e
Moon Chung <koreas1337@hotmail.com>
2021-07-16 16:39:59 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,28 +83,3 @@ resource "azurerm_kubernetes_cluster" "may24_devops_staging" {
         CreatedDate             = timestamp()
     }
 }
-
-# Provides the config file to connect to AKS cluster
-output "kube_config_dev" {
-    value = azurerm_kubernetes_cluster.may24_devops_dev.kube_config_raw
-}
-
-output "kube_config_staging" {
-    value = azurerm_kubernetes_cluster.may24_devops_staging.kube_config_raw
-}
-
-# terraform output configure
-# Probably want this in a separate outputs.tf file?
-output "configure" {
-    value = <<CONFIGURE
-
-Run the following commands to configure the kubernetes client:
-
-terraform output kube_config > ~/.kube/may24_devops_config_dev
-export KUBECONFIG=~/.kube/may24_devops_config
-
-Test configuration using kubectl:
-
-kubectl get nodes
-CONFIGURE
-}
