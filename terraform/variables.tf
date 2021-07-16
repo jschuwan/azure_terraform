12690f10a5e47ee2249116afac67d23615995ed2
Moon Chung <koreas1337@hotmail.com>
2021-07-16 15:50:07 +0000

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,17 +22,19 @@ variable "resource_group" {
 variable "kubernetes_cluster_dev" {
     type = map
     default = {
-        "name"          = "may24_devops_aks_dev"
-        "dns_prefix"    = "may24-devops-dev"
-        "node_count"    = 3
+        "name"              = "may24_devops_aks_dev"
+        "dns_prefix"        = "may24-devops-dev"
+        "node_count"        = 3
+        "node_pool_name"    = "development"
     }
 }
 
 variable "kubernetes_cluster_staging" {
     type = map
     default = {
-        "name"          = "may24_devops_aks_staging"
-        "dns_prefix"    = "may24-devops-staging"
-        "node_count"    = 3
+        "name"              = "may24_devops_aks_staging"
+        "dns_prefix"        = "may24-devops-staging"
+        "node_count"        = 3
+        "node_pool_name"    = "staging"        
     }
 }
