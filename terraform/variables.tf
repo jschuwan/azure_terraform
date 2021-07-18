bc7513eff87915d7940fd95b57d9b74dfdf003f5
Moonjun Chung <moonjchung@gmail.com>
2021-07-18 00:04:39 -0400

    Initial testing of changing import tasks

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,8 @@
-# variable "serviceprinciple_id" {}
-# variable "subscription_id" {}
-# variable "serviceprinciple_key" {}
-# variable "tenant_id" {}
-
 variable "resource_group" {
 # Replace with your own values if this is used as a template
     type = map
     default = {
         "name"      = "may24_devops"
         "location"  = "East US"
-    }
-}
-
-variable "kubernetes_cluster_dev" {
-    type = map
-    default = {
-        "name"              = "may24_devops_aks_dev"
-        "dns_prefix"        = "may24-devops-dev"
-        "node_count"        = 1
-        "node_pool_name"    = "development"
-    }
-}
-
-variable "kubernetes_cluster_staging" {
-    type = map
-    default = {
-        "name"              = "may24_devops_aks_staging"
-        "dns_prefix"        = "may24-devops-staging"
-        "node_count"        = 1
-        "node_pool_name"    = "staging"        
     }
 }
