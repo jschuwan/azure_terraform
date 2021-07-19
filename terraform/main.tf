69dfa3fae8de4364e5fd0488436e3f9fcfcd23f9
Jim Bromfield <jbromfld@gmail.com>
2021-07-19 14:39:22 -0400

    test resource limits

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,17 +20,17 @@ resource "azurerm_resource_group" "may24_devops" {
     location    = var.resource_group["location"]
 }
 
-resource "azurerm_container_registry" "may24_devops_registry" {
-    name                = var.container_registry["name"]
-    location            = azurerm_resource_group.may24_devops.location
-    resource_group_name = azurerm_resource_group.may24_devops.name
+# resource "azurerm_container_registry" "may24_devops_registry" {
+#     name                = var.container_registry["name"]
+#     location            = azurerm_resource_group.may24_devops.location
+#     resource_group_name = azurerm_resource_group.may24_devops.name
 
-  tags = {
-        Group                   = "DevOps"
-        ContactBeforeDelete     = "Nick Escalona"
-        CreatedDate             = timestamp()
-  }  
-}
+#   tags = {
+#         Group                   = "DevOps"
+#         ContactBeforeDelete     = "Nick Escalona"
+#         CreatedDate             = timestamp()
+#   }  
+# }
 
 resource "azurerm_kubernetes_cluster" "may24_devops_dev" {
     name                = var.kubernetes_cluster_dev["name"]
@@ -61,45 +61,44 @@ resource "azurerm_kubernetes_cluster" "may24_devops_dev" {
     }
 }
 
-resource "azurerm_kubernetes_cluster" "may24_devops_staging" {
-    node_count          = 0
-    name                = var.kubernetes_cluster_staging["name"]
-    location            = azurerm_resource_group.may24_devops.location
-    resource_group_name = azurerm_resource_group.may24_devops.name
-    dns_prefix          = var.kubernetes_cluster_staging["dns_prefix"]
+# resource "azurerm_kubernetes_cluster" "may24_devops_staging" {
+#     name                = var.kubernetes_cluster_staging["name"]
+#     location            = azurerm_resource_group.may24_devops.location
+#     resource_group_name = azurerm_resource_group.may24_devops.name
+#     dns_prefix          = var.kubernetes_cluster_staging["dns_prefix"]
 
-    default_node_pool {
-        name                = var.kubernetes_cluster_staging["node_pool_name"]
-        node_count          = var.kubernetes_cluster_staging["node_count"]
-        vm_size             = "Standard_DS2_v2"
-        #enable_auto_scaling = false
-    }
+#     default_node_pool {
+#         name                = var.kubernetes_cluster_staging["node_pool_name"]
+#         node_count          = var.kubernetes_cluster_staging["node_count"]
+#         vm_size             = "Standard_DS2_v2"
+#         #enable_auto_scaling = false
+#     }
 
-    role_based_access_control {
-      enabled = true
-    }
+#     role_based_access_control {
+#       enabled = true
+#     }
 
-    identity {
-        type = "SystemAssigned"
-    }
+#     identity {
+#         type = "SystemAssigned"
+#     }
 
-    tags = {
-        Group                   = "DevOps"
-        Environment             = "staging"
-        ContactBeforeDelete     = "Nick Escalona"
-        CreatedDate             = timestamp()
-    }
-}
+#     tags = {
+#         Group                   = "DevOps"
+#         Environment             = "staging"
+#         ContactBeforeDelete     = "Nick Escalona"
+#         CreatedDate             = timestamp()
+#     }
+# }
 
 output "kube_config_dev" {
     value = azurerm_kubernetes_cluster.may24_devops_dev.kube_config_raw
     sensitive = true
 }
 
-output "kube_config_staging" {
-    value = azurerm_kubernetes_cluster.may24_devops_staging.kube_config_raw
-    sensitive = true
-}
+# output "kube_config_staging" {
+#     value = azurerm_kubernetes_cluster.may24_devops_staging.kube_config_raw
+#     sensitive = true
+# }
 
 provider "kubernetes" {
   host                   = "${azurerm_kubernetes_cluster.may24_devops_dev.kube_config.0.host}"
