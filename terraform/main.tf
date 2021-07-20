5e8d39b38c68bafbab67c098675502a392faefea
Jim Bromfield <jbromfld@gmail.com>
2021-07-20 11:40:49 -0400

    changing vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,12 +93,12 @@ resource "azurerm_kubernetes_cluster" "may24_devops_staging" {
 }
 
 output "kube_config_dev" {
-    value = azurerm_kubernetes_cluster.may24_devops_dev.kube_config_raw
+    value = azurerm_kubernetes_cluster.may24_devops_aks_dev.kube_config_raw
     sensitive = true
 }
 
 output "kube_config_staging" {
-    value = azurerm_kubernetes_cluster.may24_devops_staging.kube_config_raw
+    value = azurerm_kubernetes_cluster.may24_devops_aks_staging.kube_config_raw
     sensitive = true
 }
 
