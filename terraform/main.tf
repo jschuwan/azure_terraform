fbbb65286294b3e813a6f7013b89e96512938b89
Jim Bromfield <jbromfld@gmail.com>
2021-07-20 14:47:59 -0400

    testing pipeline from main branch with pod limit

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,10 +102,105 @@ output "kube_config_staging" {
     sensitive = true
 }
 
-module "dev" {
-  source = "./modules/k8dev"
+provider "kubernetes" {
+  alias                  = "dev"
+  host                   = "${azurerm_kubernetes_cluster.may24_devops_dev.kube_config.0.host}"
+  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.may24_devops_dev.kube_config.0.client_certificate)}"
+  client_key             = "${base64decode(azurerm_kubernetes_cluster.may24_devops_dev.kube_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.may24_devops_dev.kube_config.0.cluster_ca_certificate)}"
 }
 
-module "staging" {
-  source = "./modules/staging"
+resource "kubernetes_namespace" "may24_devops_dev_t1" {
+  provider = kubernetes.dev
+  metadata {
+    labels = {
+      group = "may24-dev"
+    }
+    name = "team1"
+  }
+}
+resource "kubernetes_namespace" "may24_devops_dev_t2" {
+  provider = kubernetes.dev
+  metadata {
+    labels = {
+      group = "may24-dev"
+    }
+    name = "team2"
+  }
+}
+resource "kubernetes_namespace" "may24_devops_dev_t3" {
+  provider = kubernetes.dev
+  metadata {
+    labels = {
+      group = "may24-dev"
+    }
+    name = "team3"
+  }
+}
+
+resource "kubernetes_limit_range" "may24_devops_dev" {
+  provider = kubernetes.dev
+  metadata {
+    name = "may24-dev-resource-limits"
+  }
+  spec {
+    limit {
+      type = "Pod"
+      max = {
+        cpu    = "1000m"
+        memory = "1024Mi"
+      }
+    }
+  }
+}
+
+provider "kubernetes" {
+  alias                  = "staging"
+  host                   = "${azurerm_kubernetes_cluster.may24_devops_staging.kube_config.0.host}"
+  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.may24_devops_staging.kube_config.0.client_certificate)}"
+  client_key             = "${base64decode(azurerm_kubernetes_cluster.may24_devops_staging.kube_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.may24_devops_staging.kube_config.0.cluster_ca_certificate)}"
+}
+resource "kubernetes_namespace" "may24_devops_staging_t1" {
+  provider = kubernetes.staging
+  metadata {
+    labels = {
+      group = "may24-staging"
+    }
+    name = "team1"
+  }
+}
+resource "kubernetes_namespace" "may24_devops_staging_t2" {
+  provider = kubernetes.staging
+  metadata {
+    labels = {
+      group = "may24-staging"
+    }
+    name = "team2"
+  }
+}
+resource "kubernetes_namespace" "may24_devops_staging_t3" {
+  provider = kubernetes.staging
+  metadata {
+    labels = {
+      group = "may24-staging"
+    }
+    name = "team3"
+  }
+}
+
+resource "kubernetes_limit_range" "may24_devops_staging" {
+  provider = kubernetes.staging
+  metadata {
+    name = "may24-staging-resource-limits"
+  }
+   spec {
+    limit {
+      type = "Pod"
+      max = {
+        cpu    = "1000m"
+        memory = "1024Mi"
+      }
+    }
+  }
 }
