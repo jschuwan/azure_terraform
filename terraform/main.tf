224225cf658b4e88c86d9e70afe796d329ed24bb
Jim Bromfield <jbromfld@gmail.com>
2021-07-22 11:19:54 -0400

    refactor modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,6 @@ module "azure" {
 
 module "kubernetes" {
   source = "./modules/kubernetes"
-  kube_config = "${module.azure.azurerm_kubernetes_cluster.may24_devops_dev.kube_config}"
+  kube_config = module.azure.azurerm_kubernetes_cluster.may24_devops_dev.kube_config
 }
 
