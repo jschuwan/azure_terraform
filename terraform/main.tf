4f199e87db5000942756c8e55360ccc0b0811865
Jim Bromfield <jbromfld@gmail.com>
2021-07-22 14:27:39 -0400

    testing refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,6 +86,6 @@ module "kubernetes" {
 module "azuredevops" {
   source = "./modules/azuredevops"
 
-  resource_group = azurerm_resource_group.may24_devops.name
+  resource_group = azurerm_resource_group.may24_devops.0.name
   azurecr_name = azurerm_container_registry.may24_devops_registry.name    
 }
