38a6e8ea3bf21bfd6c929aaf637f7e35b0ed3ac3
matthew.cradit <matthew.cradit@gmail.com>
2021-07-22 21:08:06 +0000

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,15 @@
-variable "client_id" {
-    type = string
-}
-variable "client_secret" {
-    type = string
-}
-variable "tenant_id" {
-    type = string
-}
-variable "subscription_id" {
-    type = string
-}
+// variable "client_id" {
+//     type = string
+// }
+// variable "client_secret" {
+//     type = string
+// }
+// variable "tenant_id" {
+//     type = string
+// }
+// variable "subscription_id" {
+//     type = string
+// }
 
 variable "resource_group" {
 # Replace with your own values if this is used as a template
