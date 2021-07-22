a6b931af3e665117b000611e357fb5490864e77e
Jim Bromfield <jbromfld@gmail.com>
2021-07-22 15:46:01 -0400

    testing refactor

--- terraform/modules/azuredevops/main.tf
+++ terraform/modules/azuredevops/main.tf
@@ -5,14 +5,12 @@ provider "azuredevops" {
 }
 
 resource "azuredevops_serviceendpoint_azurecr" "azurecr" {
-  project_id             = "49c70608-1d8c-4622-baa7-e11442ca8dc0"
-  service_endpoint_name  = "Sample AzureCR"
+  count = "${length(var.project_id)}"
+  project_id             = "${lookup(var.project_id[count.index],"id")}"
+  service_endpoint_name  = "${lookup(var.project_id[count.index],"name")}"
   resource_group            = var.resource_group
-  azurecr_spn_tenantid      = "b8d0218e-2df0-43d0-b94d-83162050b011"
+  azurecr_spn_tenantid      = "6b63e28a-a8f9-47b5-aa40-97e231215164"
   azurecr_name              = var.azurecr_name
-  azurecr_subscription_id   = "5198ab11-bed1-4a17-9205-df35c05f87b9"
+  azurecr_subscription_id   = "e37a1117-750a-4552-bb20-e84ed6f3c85d"
   azurecr_subscription_name = "Azure subscription 1"
 }
-
-# revtenant_id: 6b63e28a-a8f9-47b5-aa40-97e231215164
-
--- terraform/modules/azuredevops/variables.tf
+++ terraform/modules/azuredevops/variables.tf
@@ -5,3 +5,25 @@ variable "resource_group" {
 variable "azurecr_name" {
     type = string
 }
+
+variable "project_id" {
+    type = list
+    default = [
+        {
+            name    = "acr-common",
+            id      = "9a591d1a-b59e-4089-a839-aaf3cf17a3a9"
+        },
+        {
+            name    = "acr-team1",
+            id      = "d6293b30-b115-4508-b81b-a6c0d733cfa0"
+        },
+        {
+            name    = "acr-team2",
+            id      = "eb90657a-0d4f-4efc-ab88-854ac581c5a8"
+        },
+        {
+            name    = "acr-team3",
+            id      = "d2963c1a-e6cb-47b0-8d51-e0051b0d446e"
+        },
+    ]
+}
