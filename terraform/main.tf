bcbee9f78e9610bb63f07ffa926b090e56b452fb
Jim Bromfield <jbromfld@gmail.com>
2021-07-22 11:23:17 -0400

    refactor modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,16 @@
-
+terraform {
+   required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "=2.60.0"
+        }
+         kubernetes = {
+            source = "hashicorp/kubernetes"
+            version = "2.3.2"
+            configuration_aliases = [ kubernetes.dev, kubernetes.staging ]
+        }
+    }
+}
 module "azure" {
   source = "./modules/azure"
 }
