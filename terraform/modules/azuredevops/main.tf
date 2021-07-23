cc9b7dfef15af16efa1a4447985dfce362f17546
Jim Bromfield <jbromfld@gmail.com>
2021-07-23 11:44:01 -0400

    adding k8 service connection

--- terraform/modules/azuredevops/main.tf
+++ terraform/modules/azuredevops/main.tf
@@ -14,3 +14,37 @@ resource "azuredevops_serviceendpoint_azurecr" "azurecr" {
   azurecr_subscription_id   = var.account_info.subscription_id
   azurecr_subscription_name = var.account_info.subscription_name
 }
+
+resource "azuredevops_serviceendpoint_kubernetes" "k8_dev" {
+  count                 = "${length(var.project_id)}"
+  project_id            = "${lookup(var.project_id[count.index], "id")}"
+  service_endpoint_name = "k8_dev_cluster"
+  apiserver_url         = "${azurerm_kubernetes_cluster.may24_devops.0.kube_config.0.host}"
+  authorization_type    = "AzureSubscription"
+
+  azure_subscription {
+    subscription_id   = var.account_info.subscription_id
+    subscription_name = var.account_info.subscription_name
+    tenant_id         = var.account_info.tenant_id
+    resourcegroup_id  = var.resource_group
+    namespace         = "${lookup(var.project_id[count.index], "namespace")}"
+    cluster_name      = "${azurerm_kubernetes_cluster.may24_devops.0.kube_config.0.name}"
+  }
+}
+
+resource "azuredevops_serviceendpoint_kubernetes" "k8_staging" {
+  count                 = "${length(var.project_id)}"
+  project_id            = "${lookup(var.project_id[count.index], "id")}"
+  service_endpoint_name = "k8_stage_cluster"
+  apiserver_url         = "${azurerm_kubernetes_cluster.may24_devops.1.kube_config.1.host}"
+  authorization_type    = "AzureSubscription"
+
+  azure_subscription {
+    subscription_id   = var.account_info.subscription_id
+    subscription_name = var.account_info.subscription_name
+    tenant_id         = var.account_info.tenant_id
+    resourcegroup_id  = var.resource_group
+    namespace         = "${lookup(var.project_id[count.index], "namespace")}"
+    cluster_name      = "${azurerm_kubernetes_cluster.may24_devops.1.kube_config.1.name}"
+  }
+}
