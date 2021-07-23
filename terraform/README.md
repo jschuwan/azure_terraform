be757c1c44632a147d22be9f92907f50caa4bced
Jim Bromfield <jbromfld@gmail.com>
2021-07-23 11:39:21 -0400

    adding revature owner token

--- terraform/README.md
+++ terraform/README.md
@@ -1,1 +1,9 @@
 # Terraform configuration for Project 3
+## Modules
+### AzureRM
+- Main module, used to create AzureRM resource groups, clusters, and container registry
+### Kubernetes
+- Module used to configure Kubernetes namespaces and resource quotas
+### AzureDevops
+- Module used to create service connections
+- Module should be uncommented in main.tf for use (authentication variables should be set in modules/azuredevops/variables.tf first)
--- terraform/modules/azuredevops/variables.tf
+++ terraform/modules/azuredevops/variables.tf
@@ -2,19 +2,16 @@
 variable "project_info" {
     type = map
     default = {
-            "url"     = "https://dev.azure.com/revature-training-uta"
-            "token"   = "foxdoargbo4wieypydpjgmkpieqf7mwf25pldnm76sufzszfc6bq"   
+            "url"       = "https://dev.azure.com/revature-training-uta"
+            "token"     = "k3o2odqc7fqbwa3fzzlfukdjg5mcqkeeltth4xcbwfwxna3x233a"
     }
 }
 
 variable "account_info" {
     type = map
     default = {
-            # "tenant_id"           = "6b63e28a-a8f9-47b5-aa40-97e231215164"
-            # "subscription_id"     = "e37a1117-750a-4552-bb20-e84ed6f3c85d"
-            # "subscription_name"   = "Azure subscription 1"
-            "tenant_id"           = "b8d0218e-2df0-43d0-b94d-83162050b011"
-            "subscription_id"     = "5198ab11-bed1-4a17-9205-df35c05f87b9"
+            "tenant_id"           = "6b63e28a-a8f9-47b5-aa40-97e231215164"
+            "subscription_id"     = "e37a1117-750a-4552-bb20-e84ed6f3c85d"
             "subscription_name"   = "Azure subscription 1"
     }
 }
